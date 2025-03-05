f0ff59d154fd269eac5a9e6574e7b85312c51931
Max Gilmour <mgilmour@maxgmr.ca>
2025-03-04 16:17:53 -0800

    Rust project structure

---
+++ src/init/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("hellloe :D");
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+//! Core library for the [tlenix](https://github.com/maxgmr/tlenix) x86_64 operating system.
+
+#![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
