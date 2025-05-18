6bba535ac138c21b549b9c0799aeab6cfe23bba8
Max Gilmour <mgilmour@maxgmr.ca>
2025-05-18 07:34:58 -0700

    Add OpenFlags

--- src/fs.rs
+++ src/fs.rs
@@ -1,5 +1,10 @@
 //! Module for filesystem operations.
 
+mod open_flags;
+
+// RE-EXPORTS
+pub use open_flags::OpenFlags;
+
 /// Process-unique identifier for a file or other input/output resource.
 /// [Wikipedia](https://en.wikipedia.org/wiki/File_descriptor)
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
