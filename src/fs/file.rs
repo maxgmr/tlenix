4be25072c51442400238015d818fc560bc3ca839
Max Gilmour <mgilmour@maxgmr.ca>
2025-05-18 22:33:54 -0700

    Impl File write

--- src/fs/file.rs
+++ src/fs/file.rs
@@ -37,7 +37,7 @@ impl File {
     }
 
     /// Reads bytes from the [`File`] into the given buffer. Returns the number of bytes read from
-    /// the file.
+    /// the file on success.
     ///
     /// This function also advances the internal file cursor.
     ///
@@ -47,7 +47,7 @@ impl File {
     /// # Errors
     ///
     /// This function returns an [`Errno`] if the underlying `read` syscall fails.
-    pub fn read(&mut self, buffer: &mut [u8]) -> Result<usize, Errno> {
+    pub fn read(&self, buffer: &mut [u8]) -> Result<usize, Errno> {
         let buf_ptr = buffer.as_mut_ptr();
 
         // SAFETY: The arguments are correct and the length is guaranteed to match the given
@@ -61,6 +61,38 @@ impl File {
                 buffer.len()
             )
         }
+    }
+
+    /// Writes bytes from the provided buffer to the given file, starting at the file's internal
+    /// cursor location. Returns the number of bytes written on success.
+    ///
+    /// If this file was opened with [`OpenOptions::append`], then the bytes will always be written
+    /// to the end of the file.
+    ///
+    /// Relies on the [`write`](https://www.man7.org/linux/man-pages/man2/write.2.html) Linux
+    /// syscall internally.
+    ///
+    /// # Errors
+    ///
+    /// This function propagates any errors encountered during the `write` syscall, returning an
+    /// [`Errno`].
+    pub fn write(&self, buffer: &[u8]) -> Result<usize, Errno> {
+        let mut total_bytes_written = 0;
+
+        while total_bytes_written < buffer.len() {
+            // SAFETY: The arguments are correct. The raw pointer to the buffer is dropped
+            // before the buffer goes out of scope. The buffer length is guaranteed to be correct.
+            total_bytes_written += unsafe {
+                syscall_result!(
+                    SyscallNum::Write,
+                    self.file_descriptor,
+                    buffer.as_ptr(),
+                    buffer.len()
+                )?
+            };
+        }
+
+        Ok(total_bytes_written)
     }
 }
 
@@ -107,7 +139,7 @@ mod tests {
     #[test_case]
     fn read_past_end() {
         let mut buffer = [0; TEST_PATH_CONTENTS.len() - 1];
-        let mut file = OpenOptions::new().open(TEST_PATH).unwrap();
+        let file = OpenOptions::new().open(TEST_PATH).unwrap();
         let bytes_read = file.read(&mut buffer).unwrap();
         let expected = &TEST_PATH_CONTENTS.as_bytes()[..TEST_PATH_CONTENTS.len() - 1];
         assert_eq!(bytes_read, buffer.len());
@@ -146,7 +178,16 @@ mod tests {
         let mut buffer = [0; 1];
         match OpenOptions::new().open("/").unwrap().read(&mut buffer) {
             Err(Errno::Eisdir) => {} // OK!
-            val => panic!("expected Err(Errno::Eisdir), got {:?}", val),
+            val => panic!("expected Err(Errno::Eisdir), got {val:?}"),
+        }
+    }
+
+    #[test_case]
+    fn write_ro() {
+        let buffer = *b"irrelevant";
+        match OpenOptions::new().open(TEST_PATH).unwrap().write(&buffer) {
+            Err(Errno::Ebadf) => {} // OK!
+            val => panic!("expected Err(Errno::Ebadf), got {val:?}"),
         }
     }
 }
