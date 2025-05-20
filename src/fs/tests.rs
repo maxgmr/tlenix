69a47bbf5c1834eb2decf74d31f85c36282b665c
Max Gilmour <mgilmour@maxgmr.ca>
2025-05-20 11:37:30 -0700

    More fs tests

--- src/fs/tests.rs
+++ src/fs/tests.rs
@@ -6,7 +6,8 @@ use super::*;
 
 const THIS_PATH: &str = "src/fs/file.rs";
 const TEST_PATH: &str = "test_files/test.txt";
-const TEST_PATH_CONTENTS: &str = "Hello! I hope you can read me without any issues! - Max (马克斯)";
+const TEST_PATH_CONTENTS: &str =
+    "Hello! I hope you can read me without any issues! - Max (马克斯)\n";
 
 #[test_case]
 fn read_bytes() {
@@ -51,9 +52,8 @@ fn read_past_end() {
     let bytes_read = file.read(&mut buffer).unwrap();
     let mut expected_2 = [0; TEST_PATH_CONTENTS.len() - 1];
     expected_2.copy_from_slice(expected);
-    expected_2[0] = TEST_PATH_CONTENTS.as_bytes()[TEST_PATH_CONTENTS.len() - 1];
-    expected_2[1] = b'\n';
-    assert_eq!(bytes_read, 2);
+    expected_2[0] = b'\n';
+    assert_eq!(bytes_read, 1);
     assert_eq!(buffer, expected_2);
 
     let bytes_read = file.read(&mut buffer).unwrap();
@@ -64,14 +64,16 @@ fn read_past_end() {
 #[test_case]
 fn read_wo() {
     let mut buffer = [0; 1];
-    match OpenOptions::new()
-        .write_only()
-        .open(TEST_PATH)
-        .unwrap()
-        .read(&mut buffer)
-    {
+    let file = OpenOptions::new().write_only().open(TEST_PATH).unwrap();
+
+    match file.read(&mut buffer) {
         Err(Errno::Ebadf) => {} // OK!
-        val => panic!("expected Err(Errno::Ebadf), got {:?}", val),
+        val => panic!("expected Err(Errno::Ebadf), got {val:?}"),
+    }
+
+    match file.read_byte() {
+        Err(Errno::Ebadf) => {} // OK!
+        val => panic!("expected Err(Errno::Ebadf), got {val:?}"),
     }
 }
 
@@ -87,7 +89,16 @@ fn read_dir() {
 #[test_case]
 fn write_ro() {
     let buffer = *b"irrelevant";
-    match OpenOptions::new().open(TEST_PATH).unwrap().write(&buffer) {
+    let byte = b'e';
+
+    let file = OpenOptions::new().open(TEST_PATH).unwrap();
+
+    match file.write(&buffer) {
+        Err(Errno::Ebadf) => {} // OK!
+        val => panic!("expected Err(Errno::Ebadf), got {val:?}"),
+    }
+
+    match file.write_byte(byte) {
         Err(Errno::Ebadf) => {} // OK!
         val => panic!("expected Err(Errno::Ebadf), got {val:?}"),
     }
@@ -115,4 +126,20 @@ fn read_advance_cursor() {
 
     let bytes_read = file.read(&mut buffer).unwrap();
     assert_eq!(file.cursor().unwrap(), bytes_read * 3);
+}
+
+#[test_case]
+fn read_byte() {
+    const EXP_LEN: usize = TEST_PATH_CONTENTS.len();
+
+    let file = OpenOptions::new().open(TEST_PATH).unwrap();
+
+    // Read the file's bytes one at a time
+    for i in 0..EXP_LEN {
+        let byte = file.read_byte().unwrap().unwrap();
+        assert_eq!(byte, TEST_PATH_CONTENTS.as_bytes()[i]);
+    }
+
+    // Make sure that we get `None` after reading to the end
+    assert!(file.read_byte().unwrap().is_none());
 }
