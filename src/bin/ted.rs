2624360064086ab778348060f0f7d0c77f380710
Max Gilmour <mgilmour@maxgmr.ca>
2025-07-24 14:44:31 -0700

    ted: Impl proper window size detection

--- src/bin/ted.rs
+++ src/bin/ted.rs
@@ -32,14 +32,22 @@ use tlenix_core::{
 
 const PANIC_TITLE: &str = "ted";
 
+const ESC_CODE: u8 = 0x1b;
+
 /// ANSI escape code to clear the entire screen.
 const CLEAR_SCREEN: &str = "\u{001b}[2J";
 /// ANSI escape code to move the cursor to the top-left corner.
 const CURSOR_TOP_LEFT: &str = "\u{001b}[H";
+/// ANSI sequence to move the cursor to the bottom-right corner.
+const CURSOR_BOTTOM_RIGHT: &str = "\u{001b}[999C\u{001b}[999B";
+/// ANSI device status report sequence to get the cursor position.
+const GET_CURSOR_POS: &str = "\u{001b}[6n";
 
 const READ_MIN_BYTES_READ: u8 = 0;
 const READ_MAX_TIME_PASSED: Deciseconds = Deciseconds(1);
 const EXIT_CODE: u8 = ctrl_key(b'q');
+
+const CHECK_TERM_RESPONSE_LIMIT: usize = 64;
 
 core::arch::global_asm! {
     ".global _start",
@@ -56,26 +64,49 @@ const fn ctrl_key(byte: u8) -> u8 {
     byte & 0x1f
 }
 
+/// A given row-column point within the screen.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+struct Point {
+    row: usize,
+    col: usize,
+}
+impl Point {
+    fn try_from_string_helper(value: &str) -> Option<Self> {
+        // Format: "[<row>;<col>R"
+        let start = value.rfind('[')? + 1;
+        let end = value.find('R')?;
+
+        let (first, second) = &value[start..end].split_once(';')?;
+
+        Some(Self {
+            row: first.parse().ok()?,
+            col: second.parse().ok()?,
+        })
+    }
+}
+impl From<Point> for WinSize {
+    fn from(value: Point) -> Self {
+        Self {
+            rows: value.row,
+            cols: value.col,
+            width: 0,
+            height: 0,
+        }
+    }
+}
+impl TryFrom<&str> for Point {
+    type Error = &'static str;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Self::try_from_string_helper(value).ok_or("failed to parse Point from string")
+    }
+}
+
 /// The current configuration of the editor.
 #[derive(Debug, Clone)]
 struct Config {
     orig_termios: Termios,
     win_size: WinSize,
-}
-impl Config {
-    fn try_new() -> Result<Self, Errno> {
-        let orig_termios = STDIN.lock().termios()?;
-        let try_win_size = STDIN.lock().win_size()?;
-        let win_size = if try_win_size.rows != 0 && try_win_size.cols != 0 {
-            try_win_size
-        } else {
-            WinSize::default()
-        };
-        Ok(Self {
-            orig_termios,
-            win_size,
-        })
-    }
 }
 
 /// A simple text editor.
@@ -107,6 +138,45 @@ unsafe extern "C" fn start(stack_top: *const usize) -> ! {
     let exit_code = main(&argv, &envp);
 
     process::exit(exit_code);
+}
+
+fn get_win_size() -> WinSize {
+    // Try to get window size from system call
+    if let Ok(ioctl_win_size) = STDIN.lock().win_size()
+        && ioctl_win_size.cols > 0
+        && ioctl_win_size.rows > 0
+    {
+        return ioctl_win_size;
+    }
+
+    // Fallback: Move the cursor to the bottom-right and get cursor position
+    print!("{CURSOR_BOTTOM_RIGHT}");
+    let win_size = if let Ok(pos) = get_cursor_pos() {
+        pos.into()
+    } else {
+        WinSize::default()
+    };
+    print!("{CURSOR_TOP_LEFT}");
+    win_size
+}
+
+/// Gets the current position of the cursor on the screen.
+fn get_cursor_pos() -> Result<Point, Errno> {
+    print!("{GET_CURSOR_POS}");
+    let mut buf = [0; CHECK_TERM_RESPONSE_LIMIT];
+    let mut stdin = STDIN.lock();
+
+    for byte in &mut buf {
+        if stdin.read(core::slice::from_mut(byte))? != 1 || *byte == b'R' {
+            break;
+        }
+    }
+
+    if buf.first() != Some(&ESC_CODE) || buf.get(1) != Some(&b'[') {
+        return Err(Errno::Enodata);
+    }
+    let response = String::from_utf8_lossy(&buf[1..]);
+    Point::try_from(response.as_ref()).map_err(|_| Errno::Einval)
 }
 
 /// Enters terminal "raw mode".
@@ -171,10 +241,15 @@ fn restore_terminal(termios: &Termios) -> Result<(), Errno> {
 
 /// Renders the rows of the interface onto the terminal.
 fn render_rows(config: &Config) {
-    for _ in 0..config.win_size.rows {
-        raw_println!("~");
+    let mut render_buf = String::with_capacity(config.win_size.rows * config.win_size.cols);
+    for i in 0..config.win_size.rows {
+        render_buf.push('~');
+        if i < config.win_size.rows - 1 {
+            render_buf.push('\r');
+            render_buf.push('\n');
+        }
     }
-    print!("{CURSOR_TOP_LEFT}");
+    print!("{render_buf}{CURSOR_TOP_LEFT}");
 }
 
 /// Refreshes the screen, displaying the intended content.
@@ -228,10 +303,16 @@ fn handle_input() -> Result<bool, Errno> {
 }
 
 fn main(_args: &[String], _env_vars: &[EnvVar]) -> ExitStatus {
-    let config = try_exit!(Config::try_new());
+    let orig_termios = try_exit!(STDIN.lock().termios());
+
     try_exit!(enter_raw_mode());
     try_exit!(set_read_timeouts(READ_MIN_BYTES_READ, READ_MAX_TIME_PASSED));
     clear_screen();
+
+    let config = Config {
+        orig_termios,
+        win_size: get_win_size(),
+    };
 
     loop {
         refresh_screen(&config);
@@ -249,4 +330,29 @@ fn main(_args: &[String], _env_vars: &[EnvVar]) -> ExitStatus {
 fn panic(info: &PanicInfo<'_>) -> ! {
     eprintln!("{PANIC_TITLE} {info}");
     process::exit(ExitStatus::ExitFailure(1))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test_case]
+    fn point_from_str_ok() {
+        assert_eq!(
+            Point::try_from("^[[12;34R").unwrap(),
+            Point { row: 12, col: 34 }
+        );
+        assert_eq!(
+            Point::try_from("\u{001b}[2;547R").unwrap(),
+            Point { row: 2, col: 547 }
+        );
+    }
+
+    #[test_case]
+    fn point_from_str_reject_bad() {
+        Point::try_from("\u{001b}[;54R").unwrap_err();
+        Point::try_from("^[[123;BR").unwrap_err();
+        Point::try_from("89;92R").unwrap_err();
+        Point::try_from("^[[12;34").unwrap_err();
+    }
 }
